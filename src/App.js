7e69975c8523efc6f160ba2ca9ed2243ff3869f2
Frank Rogneslien <frank.rogneslien@tindsoftware.no>
2017-09-03 13:31:14 +0100

    feat: added onShelfChange event

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import './App.css'
 import BookList from './BookList'
 import SearchBooks from './SearchBooks'
 
-
 class BooksApp extends React.Component {
-    state = {
-        books: []
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            books: []
+        };
     }
 
     componentDidMount() {
@@ -17,16 +20,45 @@ class BooksApp extends React.Component {
         });
     }
 
+    onShelfChange(book, shelf) {
+        if (shelf === 'none') {
+            this.setState(() => ({
+                books: this.state.books.filter((b) => b.id !== book.id)
+            }));            
+        }
+        else {
+            this.setState(() => ({
+                books: this.state.books.map((b, i) => {
+                    if (b.id === book.id) {
+                        b.shelf = shelf
+                    }
+                    return b;
+                })
+            }));
+        }
+        
+
+        
+        BooksAPI.update(book, shelf).then((book) => {
+            //success, but what about error, should catch that undo action against local array and make user aware update failed
+        });
+    }
+
     render() {
         return (
             <div className="app">
                 <Route path='/search' render={() => (
-                    <SearchBooks/>
+                    <SearchBooks
+
+                    />
                 )}
                 />
                 <Route exact path='/' render={() => (
                     <BookList 
                         books={this.state.books}
+                        onShelfChange={(book, shelf) => {
+                            this.onShelfChange(book, shelf);
+                        }}
                     />
                 )}
                 />
