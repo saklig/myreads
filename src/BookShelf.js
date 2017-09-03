30221bf4fca1b7bee8c407611bfa309aba32c527
Frank Rogneslien <frank.rogneslien@tindsoftware.no>
2017-09-03 13:30:18 +0100

    refactor: refactored some props passing style: indentation

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -2,29 +2,23 @@ import React, { Component } from 'react';
 import Book from './Book'
 
 class BookShelf extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { books: this.props.books };
-    }
-
     render() {
         return (
             <div className="bookshelf">
-            <h2 className="bookshelf-title">{this.props.title}</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {this.props.books.map((book) => (
-                    <li key={book.id}>
-                    <Book
-                        shelf={this.props.shelf}
-                        title={book.title}
-                        authors={book.authors}                        
-                        thumbnail={book.imageLinks.thumbnail}
-                    />
-                  </li>
-                ))}
-              </ol>
-            </div>
+                <h2 className="bookshelf-title">{this.props.title}</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {this.props.books.map((book) => (
+                            <li key={book.id}>
+                                <Book
+                                    shelf={this.props.shelf}
+                                    book={book}
+                                    onShelfChange={this.props.onShelfChange}
+                                />
+                            </li>
+                        ))}
+                    </ol>
+                </div>
           </div>
         );
     }
