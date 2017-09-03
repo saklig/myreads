f144ee8c7d8fb51d90a8345d074752e5e92faff2
Frank Rogneslien <frank.rogneslien@tindsoftware.no>
2017-09-03 13:27:10 +0100

    feat: switched anchor with link, passing onshelfchange prop style: indentation

--- src/BookList.js
+++ src/BookList.js
@@ -1,37 +1,40 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import BookShelf from './BookShelf'
 
 class BookList extends Component {
-    state = {  }
     render() {
         return (
             <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
+                <div className="list-books-title">
+                    <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                    <div>
+                        <BookShelf
+                            title='Currently Reading'
+                            books={this.props.books.filter((book) => book.shelf === 'currentlyReading')}
+                            shelf='currentlyReading'
+                            onShelfChange={this.props.onShelfChange}
+                        />
+                        <BookShelf
+                            title='Want to Read'
+                            books={this.props.books.filter((book) => book.shelf === 'wantToRead')}
+                            shelf='wantToRead'
+                            onShelfChange={this.props.onShelfChange}
+                        />
+                        <BookShelf
+                            title='Read'
+                            books={this.props.books.filter((book) => book.shelf === 'read')}
+                            shelf='read'
+                            onShelfChange={this.props.onShelfChange}
+                        />
+                    </div>
+                </div>
+                <div className="open-search">
+                    <Link to='/search'>Add a book</Link>
+                </div>
             </div>
-            <div className="list-books-content">
-              <div>
-                <BookShelf
-                  title='Currently Reading'
-                  books={this.props.books.filter((book) => book.shelf === 'currentlyReading')}
-                  shelf='currentlyReading'
-                />
-                <BookShelf
-                  title='Want to Read'
-                  books={this.props.books.filter((book) => book.shelf === 'wantToRead')}
-                  shelf='wantToRead'
-                />
-                <BookShelf
-                title='Read'
-                books={this.props.books.filter((book) => book.shelf === 'read')}
-                shelf='read'
-              />
-              </div>
-            </div>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
         );
     }
 }
